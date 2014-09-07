feb96e7b54d36a086f86548222a8bebd42a9801b
jbcrestot <jbcrestot@gmail.com>
2014-09-07 22:54:57 +0200

    maj

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
-var ritApiKey = '00ea28b2-19ab-4b5c-bb52-9812963e8907';
+var parameters = require('parameters.js');
 
 /* GET home page. */
 router
     .get('/', function(req, res) {
         res.render('index.html', { title: 'Express' });
     })
+    // mandatory for riot API
+    .get('riot.html', function(req, res) {
+        res.render('riot.html');
+    })
+    
     .get('/user/:userName', function(req, res) {
         
         console.log(req.userName);
