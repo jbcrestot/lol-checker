aad89e08b69d6c8122fef7d15837f1e9e573865d
jbcrestot <jbcrestot@gmail.com>
2015-10-25 19:54:38 +0100

    add summary to team ladder

--- public/javascripts/team.js
+++ public/javascripts/team.js
@@ -4,12 +4,31 @@
 $(function() {
     // listener on row
     $('.ladderTable .row').not('.first').on('click', function(event) {
-
-        console.log('click');
         // during the call
-        $(this).append('<div class="row summary" .  . ""> ... <div>')
-        $.get("/team/summary", function() {
-            // success
+        var $this = $(this);
+        $columns = $this.find('.columns');
+        // check if summary exist
+        if ($this.children('.summary').length) {
+            switchIcon($this);
+            $this.find('.summary').toggle();
+
+            return;
+        }
+
+        $.get("/team/summary", function(data, textStatus) {
+            switchIcon($this);
+            // add new line under
+            $this.append('<div class="column small-12 summary">'+ data +'</div>');
         });
     });
 });
+
+// reverse icon
+var switchIcon = function($this) {
+    $i = $this.find('.icon .fa');
+    if ($i.hasClass('fa-sort-desc')) {
+        $i.removeClass('fa-sort-desc').addClass('fa-sort-asc');
+    } else {
+        $i.removeClass('fa-sort-asc').addClass('fa-sort-desc');
+    }
+};
